12f5fe5ee251eaf8a3d9b4e5c7ea9166de9cfecb
Liam Murray <murrayl9797@gmail.com>
2020-09-11 14:46:55 -0400

    Connect local server to deployed Database

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const cors = require('cors')
 const { Client, Pool } = require('pg');
 const client = new Client({
   user: 'postgres',
-  host: 'db', // Uses docker network to connect
+  host: '34.229.137.235', // Can use docker network to connect (but not right now)
   database: 'postgres',
   password: 'password',
   port: 5432,
@@ -27,7 +27,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // app.use(cors());
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
   next();
