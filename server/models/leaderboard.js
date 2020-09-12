726f75566309a7e654a3c1cb92a05d7b3196dfca
Matthew Faris <mtfaris90@gmail.com>
2020-09-12 16:50:29 -0400

    add backend routing corrections

--- server/models/leaderboard.js
+++ server/models/leaderboard.js
@@ -3,15 +3,11 @@ const pool = require("../Pool.js");
 module.exports = {
   getUsers: () => {
     let q =
-      "SELECT username, visit_count, total_mins FROM users ORDER BY visit_count DESC LIMIT 5";
+      "SELECT first_name, last_name, visit_count, total_mins FROM users ORDER BY total_mins DESC";
     return pool
       .connect()
       .then((client) => {
         return client.query(q);
-      })
-      .then((res) => {
-        return res;
-      })
-      .catch((err) => console.log(err));
+      });
   },
 };
