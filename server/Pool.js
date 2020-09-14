e1366d0e5845bafdb2859dbef499097599bd9e5a
xdynasty <xdynasty5201314@gmail.com>
2020-09-14 18:30:39 -0400

    comment a code block to test connection to database

--- server/Pool.js
+++ server/Pool.js
@@ -1,19 +1,37 @@
 const { Pool } = require('pg');
 
 const {
-    DB_USER,
-    DB_HOST,
-    DB_DATABASE,
-    DB_PASSWORD,
-    DB_PORT,
-  } = require('./config.js');
+  DB_USER,
+  DB_HOST,
+  DB_DATABASE,
+  DB_PASSWORD,
+  DB_PORT,
+} = require('./config.js');
 
 const pool = new Pool({
-    user: DB_USER,
-    host: DB_HOST,
-    database: DB_DATABASE,
-    password: DB_PASSWORD,
-    port: DB_PORT,
+  user: DB_USER,
+  host: DB_HOST,
+  database: DB_DATABASE,
+  password: DB_PASSWORD,
+  port: DB_PORT,
 });
+
+/*
+pool.connect().then((client) => {
+  client
+    .query('select * from users;')
+    .then(({ rows }) => {
+      client.release();
+      console.log('rows:', rows);
+      console.log("CONNECTION SUCCEEDED!!! LET'S POP BOTTLES!!! WHOOOOO");
+    })
+    .catch((err) => {
+      client.release();
+      console.log(err);
+      console.log("CONNECTION FAILED!!! LET'S POP BOTTLES ANYWAY!!!");
+    });
+});
+
+*/
 
 module.exports = pool;
