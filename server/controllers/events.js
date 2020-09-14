0816d8e7737aa6d0137ef86e4315ed205febe498
mengel142 <matthew.engel88@gmail.com>
2020-09-14 15:28:02 -0400

    create events models and controllers

--- server/controllers/events.js
+++ server/controllers/events.js
@@ -12,16 +12,22 @@ module.exports = {
     },
 
     postEvent: (req, res) => {
+        console.log('post event in controllers');
         const { host, title, description, location, start, end} = req.body;
         events
         .postEvent(host, title, description, location, start, end)
-        .then(() => {
-            return events.getAllEvents()
-        })
-        .then(({rows}) => res.send(rows))
+        .then(() => res.sendStatus(204))
         .catch(err => {
-            console.log('getAll events error', err);
-            res.sendStatus(404);
+            console.log(err);
+            res.sendStatus(500);
         })
+        // .then(() => {
+        //     return events.getAllEvents()
+        // })
+        // .then(({rows}) => res.send('working'))
+        // .catch(err => {
+        //     console.log('getAll events error', err);
+        //     res.sendStatus(404);
+        // })
     }
 }
--- server/models/events.js
+++ server/models/events.js
@@ -6,31 +6,36 @@ module.exports = {
         return pool.connect()
             .then(client => {
                 return client.query(q)
+                .then(res => {
+                    client.release();
+                    return res;
+                })
+                .catch(err => {
+                    client.release();
+                    console.log(err.stack);
+                })
             })
-            .then(res => {
-                client.release();
-                return res;
-            })
-            .catch(err => {
-                client.release();
-                console.log(err.stack);
-            })
+            
     },
 
     postEvent: (host, title, description, location, start, end) => {
+        console.log('post request in models...');
         let q = 'INSERT INTO events (host, title, description, location, start_time, end_time) VALUES ($1, $2, $3, $4, $5, $6)';
+        console.log('pool: ', pool.connect());
         return pool.connect()
             .then(client => {
-                return client.query(q, [host, title, description, location, start, end]);
+                return client.query(q, [host, title, description, location, start, end])
+                // .then(res => {
+                //     console.log('post query');
+                //     client.release();
+                //     return res;
+                // })
+                // .catch(err => {
+                //     client.release();
+                //     console.log(err.stack);
+                // })
             })
-            .then(res => {
-                client.release();
-                return res;
-            })
-            .catch(err => {
-                client.release();
-                console.log(err.stack);
-            })
+            
     },
 
     select: (id) => {
