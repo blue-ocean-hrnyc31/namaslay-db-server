34b760e5aa5c54bbfa9ed49cef6d022ddb42ccf7
Deo-Mac <druplall@gmail.com>
2020-09-15 15:39:34 -0400

    Added release to leader board route

--- server/models/leaderboard.js
+++ server/models/leaderboard.js
@@ -4,8 +4,22 @@ module.exports = {
   readLeaders: () => {
     let q =
       'SELECT first_name, last_name, visit_count, total_mins FROM users ORDER BY total_mins DESC';
-    return pool.connect().then((client) => {
-      return client.query(q);
-    });
+    return pool
+      .connect()
+      .then((client) => {
+        return client
+          .query(q)
+          .then((res) => {
+            client.release();
+            return res;
+          })
+          .catch((err) => {
+            client.release();
+            console.log(err);
+          });
+      })
+      .catch((err) => {
+        console.log('Error getting leaderboard data: ', err);
+      });
   },
 };
