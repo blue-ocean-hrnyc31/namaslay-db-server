c7a6719d17dea562b471c3f24967fb81c7c6a33f
Nuri Kim <nuri.g.kim@gmail.com>
2020-09-15 21:14:22 -0400

    Add visit_count increment functionality.

--- server/models/river.js
+++ server/models/river.js
@@ -17,7 +17,8 @@ module.exports = {
     let queryStr = `UPDATE users
     SET current_river = $2,
     current_activity= $3,
-    total_mins = total_mins + $4
+    total_mins = total_mins + $4,
+    visit_count = visit_count + 1
     WHERE user_id = $1`;
     return pool
       .connect()
